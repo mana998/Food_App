f00d2ea2ca33b504e5aa8822e3c63e2b4251f131
Dagmara <przygocka.dagmara@gmail.com>
2021-05-03 20:56:55 +0200

    mysql database connected

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+//get the connection to db so yuo can run queries, connection fefined in folder database file connection.js
+const db = require("./database/connection").connection; 
+
+
 app.use(express.json());
 //allow to pass form data
 app.use(express.urlencoded({ extended: true}));
@@ -19,6 +23,37 @@ const recipes = fs.readFileSync(__dirname + '/public/recipes/recipes.html', 'utf
 app.get("/recipes", (req, res) => {
     res.send(recipes);
 });
+
+
+//database example queries!! for USER table it will be run every time you run app.js
+
+/*
+
+//create check if user already exists
+db.query(`INSERT INTO user (username, password, active) VALUES (?, ?, ?);`, ["Perdro", "Password",1], (error, result, fields) => {
+    console.log(result);
+});
+
+
+//read
+db.query('SELECT * FROM user;', (error, result, fields) => {
+    console.log(result);
+});
+
+
+//update, we should check first if username already exists
+db.query('UPDATE user SET username = "Dan", password="SecurePassword"  WHERE user_id = 1', (erro, result, fields) => {
+    console.log(result);
+})
+
+
+//delete
+db.query("DELETE FROM user WHERE username = 'Dan'", (error, result, fields) => {
+    console.log(result);
+})
+;
+
+*/
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
     if (error) {
