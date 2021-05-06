f267ce1ae3a3fc3001f381e34dd014b2222e5518
Dagmara <przygocka.dagmara@gmail.com>
2021-05-06 21:21:17 +0200

    recie design and pass date from db

--- public/recipe/recipe.js
+++ public/recipe/recipe.js
@@ -1,18 +1,41 @@
-async function renderRecipe(recipeName) {
-    let fetchRecipe = `/api/recipe/${recipeName}`;
+async function renderRecipe() {
+    let fetchRecipe = `/api${window.location.pathname}`;
     const response = await fetch(fetchRecipe);
     const result = await response.json();
 
-   
+    if (result.message){
+        $(".flex-container").append(`<h1 class="page-title" >${result.message}</h1>`);
+    }
     
+    $(".flex-container").append(`<h1 class="page-title" >${result.recipe.name}</h1>`);
+
     $(".flex-container").append(`
-        <h1>${result.recipe.name}</h1>
-        <div class="col-12 image-container">
+    <div id ="recipe-container" class="row">
+        <div class="col-12 col-lg-8">
             <img class="recipe-img" src="./../recipes/img/chocolate cake.png" alt="${result.recipe.name} image"}>
         </div>
-        <div class="col-12">
-            ${result.recipe.description}
-        </div>
-    `);
+        <div id ="ingredients" class=" col-12 col-lg-4 "></div>
+    </div>`);
+
+
+    $("#ingredients").append(`<h3 class="ingredients-headder">Ingredients List</h3><table class="table table-hover"><tbody></tbody></table>`)
+
+    for(const i in result.ingredients){
+
+        $(".table").append(`
+        <tr>
+            <th scope="col">${ result.ingredients[i].name}</th>
+            <th scope="col">${ result.ingredients[i].amount}</th>
+            <th scope="col">${ result.ingredients[i].measure}</th>
+        </tr>`)
+    }
+
+    $(".flex-container").append(`
+    <div id = "descripton">
+        <h3 class="description-headder">Method</h3>
+    </div>`);
+
+    result.recipe.description.split(".").forEach(line => {
+        $("#descripton").append(`<p>${line}.</p>`)})
 
 };
--- routes/recipe.js
+++ routes/recipe.js
@@ -9,22 +9,44 @@ class Recipe {
         //there should be url in db form img 
     }
 }
+class Ingredient{
+    constructor (name, measure, amount) {
+        this.name = name;
+        this.measure = measure;
+        this.amount = amount;
+ 
+    }
+}
 
 
 
 router.get("/api/recipe/:recipe_name", (req, res) => {
     
     //get recipe from db
-    
-    db.query('SELECT * FROM recipe WHERE recipe_name=? ;',[req.params.recipe_name], (error, result, fields) => {
-    //this part should be outside
-        if (result){
-            //write recipe to object
-            const recipe = new Recipe(result[0].recipe_name, result[0].recipe_desc, result[0].user_id );
 
-            res.send({recipe});
+
+    db.query('SELECT * FROM recipe INNER JOIN ingredient_has_recipe ON recipe.recipe_id = ingredient_has_recipe.recipe_id INNER JOIN ingredient ON ingredient_has_recipe.ingredient_id = ingredient.ingredient_id INNER JOIN measurement ON ingredient.measurement_id = measurement.measurement_id WHERE recipe.recipe_name=?;',[req.params.recipe_name], (error, result, fields) => {
+        
+
+        //this part should be outside
+        if (result.length != 0){
+            console.log(result)
+            //write recipe to object
+            const ingredients = [];
+            for (const ingredient in result){
+
+                ingredients.push(new Ingredient(result[ingredient].ingredient_name, result[ingredient].measurement_name, result[ingredient].amount));
+            }
+            const recipe = new Recipe(result[0].recipe_name, result[0].recipe_desc, result[0].user_id );
+            res.send({
+                        recipe: recipe,
+                        ingredients: ingredients
+                    });
         }else{
-            res.redirect("/recipes");
+   
+            res.send({
+                message: "This recipe does not exists."
+            });
         }
     });
  
