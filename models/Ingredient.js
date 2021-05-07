a25bd31bff9448b9ad36c3b9e77f9bcf901460d6
Marianna <mana998@azet.sk>
2021-05-07 18:22:14 +0200

    Created separate js files for classes

---
+++ models/Ingredient.js
@@ -0,0 +1,12 @@
+class Ingredient{
+    constructor (name, measure, amount) {
+        this.name = name;
+        this.measure = measure;
+        this.amount = amount;
+ 
+    }
+}
+
+module.exports = {
+    Ingredient: Ingredient
+}
