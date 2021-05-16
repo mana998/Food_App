4b1c4dab3b053f7b756465ca120982ed20a756f1
Marianna <mana998@azet.sk>
2021-05-16 15:08:37 +0200

    Fixed filtering query for recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,29 +6,17 @@ const Recipe = require("./../models/Recipe").Recipe;
 router.get("/api/recipes", (req, res) => {
 
     let page = req.query.page || 1;
-    let size = req.query.size;
+    let size = req.query.size || 1000;
     //add filtering
-    let filter = req.query.filter;
-    let user_id =req.query.user_id;
+    let filter = req.query.filter || "likes";
+    let direction = req.query.direction || "desc";
 
-    let query = "";
-    let values = [];
-
-    if (user_id){
-        query = 'SELECT recipe_name, recipe_img FROM recipe WHERE recipe.user_id = ?;';
-        values = [user_id];
-    }else if (filter ==="favorites"){
-        query = 'SELECT recipe_name, recipe_img FROM recipe ;';
-    }
-    else {
-        query = 'SELECT recipe_name, recipe_img FROM recipe LIMIT ? OFFSET ?;';
-        values = [Number(size), Number((page - 1) * size)];
-    }
-
+    //needs to be in there as else it is putting it into quotes
+    let query = `SELECT recipe_name, recipe_img FROM recipe ORDER BY ${filter} ${direction} LIMIT ? OFFSET ?;`;
+    values = [Number(size), Number((page - 1) * size)];
 
     db.query(query, values, (error, result, fields) => {
-    
-        //this part should be outside
+        //console.log("result", result);
         if (result && result.length) {
             //write recipe to object
             const recipes = [];
